79ffca6980c0d6d7bc3c96d49597e3faf60f6f77
Bekzat707 <bekzatsaparbekov070@gmail.com>
2024-09-13 22:39:59 +0500

    Create 2.sql

---
+++ lab1/2.sql
@@ -0,0 +1,5 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,       -- Auto-incrementing integer
+    firstname VARCHAR(50),       -- String with max length 50
+    lastname VARCHAR(50)         -- String with max length 50
+);
