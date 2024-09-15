e19f71569289867d9b602e6c84cccd3e310145b0
Bekzat707 <bekzatsaparbekov070@gmail.com>
2024-09-15 14:04:30 +0500

    Delete 7.sql

--- lab1/7.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE tasks (
-    id SERIAL PRIMARY KEY,   -- Auto-incrementing integer
-    name VARCHAR(50),        -- String with a maximum length of 50 characters
-    user_id INTEGER          -- Integer referencing user
-);
